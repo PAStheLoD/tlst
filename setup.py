c55c625f5f6809e5dee53368bfb8f1ace2934bb6
Pas <pasthelod@gmail.com>
2015-04-30 03:53:15 +0200

    fixup

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='tlst',
